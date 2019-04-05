49daea810cfb009d0fc376de271bed5639e5b24e
Antoine DESSAIGNE <antoine.dessaigne@gmail.com>
2019-04-05 13:52:24 +0200

    Move key handling in install script

--- ci/travis-install.sh
+++ ci/travis-install.sh
@@ -4,3 +4,7 @@ set -e
 echo "Configuring maven setting.xml"
 mkdir -p ~/.m2
 cp ci/settings.xml ~/.m2/settings.xml
+
+echo "Configuring keys"
+echo "$GPG_SECRET_KEYS" | base64 --decode | gpg --import
+echo "$GPG_OWNERTRUST" | base64 --decode | gpg --import-ownertrust
--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -10,11 +10,6 @@ if [ "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_PULL_REQUEST" = "false" -a "
     echo "Build, test and generate Sonar report"
     mvn -B clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar
 
-    echo
-    echo "Decrypting GPG files"
-    openssl aes-256-cbc -d -k "$OPENSSL_PASSPHRASE" -in ci/pubring.gpg.enc -out ci/pubring.gpg
-    openssl aes-256-cbc -d -k "$OPENSSL_PASSPHRASE" -in ci/secring.gpg.enc -out ci/secring.gpg
-
     if [ \("$TRAVIS_BRANCH" = "master" -o "$TRAVIS_TAG" != ""\) -a "$OSSRH_USERNAME" != "" ]; then
         echo
         echo "Build, sign with GPG and deploy to OSSRH"
